329f3cd992c2e6e7fd0ec754c41627283fba61c4
jmgf11 <45073003+jmgf11@users.noreply.github.com>
2020-06-15 23:13:22 -0400

    tile map

---
+++ tile map/jungle tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="jungle tileset" tilewidth="16" tileheight="16" tilecount="1104" columns="48">
+ <image source="../Assets jungla/Jungle Asset Pack/jungle tileset/jungle tileset.png" width="768" height="368"/>
+</tileset>
